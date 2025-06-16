37ef9ba524dc8a72e96484db5b0dfac89c262135
JeremKy <jeremky@me.com>
2025-06-16 17:09:51 +0200

    Mise à jour

--- hugoinstall.sh
+++ hugoinstall.sh
@@ -10,6 +10,7 @@ fi
 # Téléchargement et installation
 version=$(curl -s -L -o /dev/null -w '%{url_effective}' https://github.com/gohugoio/hugo/releases/latest | sed 's#.*/tag/v##')
 mkdir -p ~/.local/bin
-curl -LO https://github.com/gohugoio/hugo/releases/download/v"$version"/hugo_extended_"$version"_linux-"$arch".tar.gz
+curl -LOs https://github.com/gohugoio/hugo/releases/download/v"$version"/hugo_extended_"$version"_linux-"$arch".tar.gz
 tar -C ~/.local/bin -xzf hugo_extended_"$version"_linux-"$arch".tar.gz
 rm hugo_extended_"$version"_linux-"$arch".tar.gz ~/.local/bin/LICENSE ~/.local/bin/README.md
+echo "Installation de hugo terminée"
